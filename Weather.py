d7fdf918d36beb4ffe211a021743c8fa1820c6de
PoojaKorde2000 <134185669+PoojaKorde2000@users.noreply.github.com>
2023-07-27 21:19:14 +0530

    Weather.py

--- Weather.py
+++ Weather.py
@@ -1,25 +1,16 @@
-# Function to get weather data from the API for a given date
+
 def get_weather_data(date):
-    # Replace this with the actual API call to get temperature data for the input date
-    # For example: Make an API call to fetch temperature data based on the date
-    temperature = 25  # Replace this with the actual temperature data from the API
+    temperature = 25 
     return temperature
 
-# Function to get wind speed data from the API for a given date
 def get_wind_speed_data(date):
-    # Replace this with the actual API call to get wind speed data for the input date
-    # For example: Make an API call to fetch wind speed data based on the date
-    wind_speed = 10  # Replace this with the actual wind speed data from the API
+    wind_speed = 10  
     return wind_speed
 
-# Function to get pressure data from the API for a given date
 def get_pressure_data(date):
-    # Replace this with the actual API call to get pressure data for the input date
-    # For example: Make an API call to fetch pressure data based on the date
-    pressure = 1012  # Replace this with the actual pressure data from the API
+    pressure = 1012 
     return pressure
 
-# Main program
 def main():
     while True:
         print("Options:")
